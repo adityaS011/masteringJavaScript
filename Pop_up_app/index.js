f37619addeefb4118781bde06d125ce47e0aedf0
adityaS011 <adityasinghtech011@gmail.com>
2023-10-27 01:32:08 +0530

    end

--- Pop_up_app/index.js
+++ Pop_up_app/index.js
@@ -1,5 +1,10 @@
 let popup = document.getElementById("popup");
 
 const openPopUp=()=>{
+    popup.classList.add("open-popup");
+
+}
+const closePopUp=()=>{
+    popup.classList.remove("open-popup");
     
 }
