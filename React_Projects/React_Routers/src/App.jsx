cc0a920d5d5d858cd9dc7fb96a05aab2fedf1283
adityaS011 <adityasinghtech011@gmail.com>
2023-12-23 18:39:23 +0530

    router-react

---
+++ React_Projects/React_Routers/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+const App = () => {
+  return (
+    <>
+    <Header/>
+      <Home/>
+      <Footer/>
+      </>
+  )
+}
+
+export default App
