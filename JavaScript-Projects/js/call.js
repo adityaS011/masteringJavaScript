2c43624095cef874ec8ca13cacb86d2b0cc50bc9
adityaS011 <adityasinghtech011@gmail.com>
2024-03-01 17:32:33 +0530

    object call

---
+++ JavaScript-Projects/js/call.js
@@ -0,0 +1,17 @@
+// this access local & windows 
+
+function SetUsername(username){
+    this.username= username;
+    console.log("called");
+}
+
+function createUser(username, email, password){
+    // SetUsername.call(username); its called but instanly removed from execution context
+    SetUsername.call(this, username); // this is used for refrence
+    this.email= email;
+    this.password= password;
+
+}
+
+const chai = new createUser("chai", "chai@gmail.com", "chaiTea");
+console.log(chai);
