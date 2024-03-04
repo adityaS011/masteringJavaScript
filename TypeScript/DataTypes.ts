c9058057535225b11e417e9747b3fe7eb54b43a9
adityaS011 <adityasinghtech011@gmail.com>
2024-03-05 02:30:06 +0530

    DataTypes

--- TypeScript/DataTypes.ts
+++ TypeScript/DataTypes.ts
@@ -21,7 +21,8 @@ console.log(tuple)
 
 // Enum - enumeration of named  values with explicit type
 enum Color {Red, Green, Blue}
-Color.Green;
+console.log(Color[0]);
+
 
 // Any - can be anything, use sparingly
 let notSure: any = 4
@@ -34,9 +35,7 @@ function isItAny(value: any): void{
     }
 }
 
-// Object - dynamic property names
-const obj: object = {}
-obj['property'] = 'Value'
+
 
 // Type Assertion - tell the compiler that you know better than it does
 const someValue: unknown = 'this is a string'
@@ -47,9 +46,9 @@ if (typeof someValue === 'string') {
 }
 
 const definitelyAString: string = <string>someValue
-console.log(definitelyAString) 
+// console.log(definitelyAString) 
 
-// interfaces in typescript is a way to create objects with specific datatypes
+// interfaces in typescript is a way to create your own type with object 
 interface IUser {
     name: string,
     age?: number // optional property is defined by ?:
@@ -57,6 +56,8 @@ interface IUser {
 
 const user: IUser = {name:'John'}
 user.age = 30
+
+
 
 // Type Guards - runtime type checking using interfaces or types
 function printName(user: IUser){
