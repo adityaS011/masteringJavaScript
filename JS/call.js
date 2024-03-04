dcd7fb5143c5980898b41e12ce5ee6875c158ada
adityaS011 <adityasinghtech011@gmail.com>
2024-03-05 02:20:31 +0530

    call

--- JS/call.js
+++ JS/call.js
@@ -8,10 +8,14 @@ function SetUsername(username){
 function createUser(username, email, password){
     // SetUsername.call(username); its called current from execution context
     SetUsername.call(this, username); // this is used for refrence
+    SetUsername.apply(this, [username]); // in apply second argument is an array
     this.email= email;
     this.password= password;
 
 }
+// const chai = createUser.bind(SetUsername); // in apply second argument is an array
+//     chai() // bind both functions together
+
 
 const chai = new createUser("chai", "chai@gmail.com", "chaiTea");
 console.log(chai);
